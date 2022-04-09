503099a53a8c69b5e1d8a37f3551a4395d02a946
Mr.shen <2733656558@qq.com>
2022-04-09 21:04:14 +0800

    feat:完成首页es相关功能

---
+++ product/src/main/java/com/sxx/product/web/ItemController.java
@@ -0,0 +1,40 @@
+package com.sxx.product.web;
+
+import com.sxx.product.service.SkuInfoService;
+import com.sxx.product.vo.SkuItemVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ExecutionException;
+
+
+
+/**
+ * @author admin
+ */
+@Controller
+public class ItemController {
+
+    @Autowired
+    private SkuInfoService skuInfoService;
+
+    /**
+     * 展示当前sku的详情
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/{skuId}.html")
+    public String skuItem(@PathVariable("skuId") Long skuId, Model model) throws ExecutionException, InterruptedException {
+
+        System.out.println("准备查询" + skuId + "详情");
+
+        SkuItemVo vos = skuInfoService.item(skuId);
+        
+        model.addAttribute("item",vos);
+
+        return "item";
+    }
+}
